e2d99029474cb2a1a8b518fe84689b63f7be86cf
liamevansdc0011-alt <liamevansdc0011@gmail.com>
2026-09-10 16:14:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -173,9 +173,9 @@ app.post('/api/send-stream', async (req, res) => {
             }
         });
 
-        // 2-second delay between 12-mail batches = Exact ~10-12s total duration for 24 emails
+        // 2-second delay between 8-mail batches = Exact ~10-12s total duration for 24 emails
         if (i + BATCH_SIZE < recipients.length) {
-            await new Promise((resolve) => setTimeout(resolve, 1800));
+            await new Promise((resolve) => setTimeout(resolve, 2000));
         }
     }
 
